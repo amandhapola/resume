2d9bace232e56e1188c201b051e36d6f698adf05
Aman Dhapola <amandhapola@gmail.com>
2015-04-24 23:47:08 +0530

    added a comment to remove resume from path

--- core/init.php
+++ core/init.php
@@ -2,6 +2,8 @@
 session_start();
 // error_reporting(1);
 // error_reporting(E_ALL);
+
+//just delete the 'resume' from the path and it will work 
 if(file_exists($_SERVER['DOCUMENT_ROOT'].'/resume/core/database/connect.php'))
 {
 require($_SERVER['DOCUMENT_ROOT'].'/resume/core/database/connect.php');
